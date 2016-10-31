995d862b2e91e08fc6158d04e1d1f6519f9091a8
slaceved <sonya.stauffer@gmail.com>
2016-10-31 01:29:55 -0700

    updates

---
+++ KaprekarNumber.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+
+public class KaprekarNumber
+{
+    public List<int> Data { get; private set; }
+    public int Min { get; set; }
+    public int Max { get; set; }
+
+    public void PrintKaprekarNumber()
+    {
+        if (Min <= 0 || Max <= 0 || Min > Max) return;
+        Data = new List<int>();
+        for (var idx = Min; idx <= Max; idx++)
+        {
+            var squar = (long)Math.Pow(idx, 2);
+            var squarString = squar.ToString();
+
+            //for one digital,prefix with "0"
+            if (squar <= 9)
+                squarString = $"0{squarString}";
+
+            var midIdx = squarString.Length / 2;
+            var leftSubstring = squarString.Substring(0, midIdx);
+            var rightSubstring = squarString.Substring(midIdx);
+
+            var leftSubInt = long.Parse(leftSubstring);
+            var rightSubInt = long.Parse(rightSubstring);
+
+            if (leftSubInt + rightSubInt == idx)
+            {
+                Data.Add(idx);
+            }
+        }
+    }
+}
---
+++ QuickSort.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+    internal class Quicksort
+    {
+        private static void Swap(IList<int> array, int left, int right)
+        {
+            var temp = array[right];
+            array[right] = array[left];
+            array[left] = temp;
+        }
+
+        public void Sort(int[] array, int left, int right)
+        {
+            while (true)
+            {
+                var lHold = left;
+                var rHold = right;
+                var objRan = new Random();
+                var pivot = objRan.Next(left, right);
+                Swap(array, pivot, left);
+                pivot = left;
+                left++;
+
+                while (right >= left)
+                {
+                    if (array[left] >= array[pivot] && array[right] < array[pivot])
+                        Swap(array, left, right);
+                    else if (array[left] >= array[pivot])
+                        right--;
+                    else if (array[right] < array[pivot])
+                        left++;
+                    else
+                    {
+                        right--;
+                        left++;
+                    }
+                }
+                Swap(array, pivot, right);
+                pivot = right;
+                if (pivot > lHold)
+                    Sort(array, lHold, pivot);
+                if (rHold > pivot + 1)
+                {
+                    left = pivot + 1;
+                    right = rHold;
+                    continue;
+                }
+                break;
+            }
+        }
+    }
